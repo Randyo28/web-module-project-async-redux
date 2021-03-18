6073f956dd2b077cbff62a602c82df1713bf6664
Randy Ortiz <Randyortiz28@gmail.com>
2021-03-18 01:43:13 -0400

    Added beerItem feature

--- punk-beer/src/components/BeerInfo.js
+++ punk-beer/src/components/BeerInfo.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import { connect } from 'react-redux'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
 
 const BeerInfo = (props) => {
-    const {name}= props.beers
+
+    const [beerInfo, setBeerInfo] = useState({})
+
+    const {id} = useParams()
+
+    useEffect(() => {
+      axios
+      .get(`https://api.punkapi.com/v2/beers/${id}`)
+      .then(res => {
+          console.log(res.data[0])
+          setBeerInfo(res.data[0])
+      })
+      .catch(err => {
+          console.log(err)
+      })
+    }, [id]);
+
+    const {name, image_url, abv, description, tagline} = beerInfo;
 
     return (
         <div>
             <h1>{name}</h1>
+            <img className='beer-image' src={image_url} alt='beer'/>
+            <h1>{description}</h1>
+            <h1>{abv}</h1>
+            <h1>{tagline}</h1>
         </div>
     );
 };
 
-const mapStateToProps = (state) => {
-    return {
-    beers: state.beers,
-    }
-  }
-
-export default connect(mapStateToProps)(BeerInfo);
+export default BeerInfo;
